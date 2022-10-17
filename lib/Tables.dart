6eb6f64fc5fc3c691ccaa3362805e5cb32d43016
musafarahamed <musafarahamed17@gmail.com>
2022-10-17 18:08:42 +0530

    samplejuly commit

--- lib/Tables.dart
+++ lib/Tables.dart
@@ -10,51 +10,48 @@ class MyTable extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-        home: DataTable(
-          columns: const <DataColumn>[
-            DataColumn(
-              label: Text(
-                'Course',
-                style: TextStyle(fontStyle: FontStyle.italic),
-              ),
-            ),
-            DataColumn(
-              label: Text(
-                'Collage',
-                style: TextStyle(fontStyle: FontStyle.italic),
-              ),
-            ),
-            DataColumn(
-              label: Text(
-                'Branch',
-                style: TextStyle(fontStyle: FontStyle.italic),
-              ),
-            ),
-          ],
-          rows: const <DataRow>[
-            DataRow(
-              cells: <DataCell>[
-                DataCell(Text('B.TECH')),
-                DataCell(Text('MMMUT')),
-                DataCell(Text('CSE')),
+      home: Scaffold(
+          body: ListView(children: <Widget>[
+            const Center(
+                child: Text(
+                  'Table',
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                )),
+            DataTable(
+              columns: const [
+                DataColumn(label: Text(
+                    'Course',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)
+                )),
+                DataColumn(label: Text(
+                    'Collage',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)
+                )),
+                DataColumn(label: Text(
+                    'Branch',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)
+                )),
+              ],
+              rows: const [
+                DataRow(cells: [
+                  DataCell(Text('B-TECH')),
+                  DataCell(Text('MMUT')),
+                  DataCell(Text('CSE')),
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('B-TECH')),
+                  DataCell(Text('KTU')),
+                  DataCell(Text('IT')),
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('B-TECH')),
+                  DataCell(Text('IET')),
+                  DataCell(Text('ME')),
+                ]),
               ],
             ),
-            DataRow(
-              cells: <DataCell>[
-                DataCell(Text('B.TECH')),
-                DataCell(Text('HBTU')),
-                DataCell(Text('IT')),
-              ],
-            ),
-            DataRow(
-              cells: <DataCell>[
-                DataCell(Text('B.TECH')),
-                DataCell(Text('IET')),
-                DataCell(Text('ME')),
-              ],
-            ),
-          ],
-        ));
+          ])
+      ),
+    );
   }
 }
